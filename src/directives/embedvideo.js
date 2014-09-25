7261fa9b9de00a342fd3a00e0189b2daf6510167
Casear Chu <casearchu@gmail.com>
2014-09-25 13:35:07 +0800

    add window focus

--- src/directives/embedvideo.js
+++ src/directives/embedvideo.js
@@ -1,13 +1,39 @@
-angular.module('videosharing-embed').directive('embedVideo', [ '$filter' , 'RegisteredPlayers', '$sce', function ($filter, RegisteredPlayers, $sce) {
+angular.module('videosharing-embed').directive('embedVideo', [ '$filter' , 'RegisteredPlayers', '$sce','$interval','$window', function ($filter, RegisteredPlayers, $sce,$interval,$window) {
 	'use strict';
     return {
         restrict: "E",
-        template: '<iframe width="{{width}}" height="{{height}}" data-ng-src="{{trustedVideoSrc}}" allowfullscreen frameborder="0"></iframe>',
+        template: '<iframe width="{{width}}" height="{{height}}"  data-ng-src="{{trustedVideoSrc}}" allowfullscreen frameborder="0"></iframe>',
         scope: {
             height: '@',
-            width: '@'
+            width: '@',
+
         },
         link: function ($scope, $element, $attrs) {
+            var iframe = e[0].children[0]
+            if (iframe.attachEvent){
+                iframe.attachEvent("onload", function(){
+                    $scope.i = i(function(){
+                        $window.focus()
+                    },500)
+                });
+            } else {
+                iframe.onload = function(){
+                    $scope.i = i(function(){
+                        $window.focus()
+                    },500) 
+                };
+            }
+            $element.on('$destroy', function(){
+                if($scope.i)
+                {
+                    $interval.cancel($scope.i)
+                    $scope.i = null
+                }
+            });
+            $scope.key = function(event){
+              event.preventDefault()
+              console.log('log')
+            }
             $attrs.$observe('width', function(w) {
               $scope.width = w;
             });
@@ -21,6 +47,12 @@ angular.module('videosharing-embed').directive('embedVideo', [ '$filter' , 'Regi
               if(url === undefined)
                 return;
               var player = null;
+
+              if(url==='' || url == null){
+                $scope.trustedVideoSrc = "";
+                return;
+              }
+
               //search for the right player in the list of registered players
               angular.forEach(RegisteredPlayers, function (value) {
                 if (value.isPlayerFromURL(url)) {
@@ -30,6 +62,7 @@ angular.module('videosharing-embed').directive('embedVideo', [ '$filter' , 'Regi
               if(player === null)
                   return; //haven't found a match for a valid registered player
               //get the videoID
+
               var videoID = url.match(player.playerRegExp)[2];
 
               //copy configuration from player
